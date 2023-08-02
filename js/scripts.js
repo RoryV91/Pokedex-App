55e2da51f9dca0f8306cb0d6252e35078ba0a396
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2023-08-01 22:41:20 -0700

    Update scripts.js
    created array of 3 pokémon objects

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+let pokémonList = [
+    {
+        name: 'Zubat', 
+        height: 0.8,
+        types: [
+            'Poison', 
+            'Flying'
+        ],
+    },
+    {
+        name: 'Growlithe',
+        height: 0.7,
+        types: [
+            'Fire'
+        ]
+    },
+    {
+        name: 'Cubone',
+        height: 0.4,
+        types: [
+            'Ground'
+        ]
+    }
+
+]
