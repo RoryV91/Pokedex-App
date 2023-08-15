9e168b220fab15832196d06f99634ff9ecada9ef
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2023-08-14 22:13:49 -0700

    Update scripts.js
    refactored for IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,54 @@
-let pokemonList = [
-    {
-        name: 'Zubat', 
-        height: 0.8,
-        types: [
-            'Poison', 
-            'Flying'
-        ],
-    },
-    {
-        name: 'Growlithe',
-        height: 0.7,
-        types: [
-            'Fire'
-        ]
-    },
-    {
-        name: 'Cubone',
-        height: 0.4,
-        types: [
-            'Ground'
-        ]
-    },
-    {
-        name: 'Aerodactyl',
-        height: 1.8,
-        types: [
-            'Flying',
-            'Rock'
-        ]
-    }
-
-]
-// PREV. EXERCISE
-// for (let i=0; i<pokemonList.length; i++) {
-//     if (pokemonList[i].height > 1.0) {
-//            document.write(pokemonList[i].name + "." + ` Height: ${pokemonList[i].height}` + " Wow, that’s big!"+ "<br>") 
-//     } else {
-//         document.write(pokemonList[i].name + "." + ` Height: ${pokemonList[i].height}` +  "<br>")
-//     }
-// }
-
-pokemonList.forEach(function(pokemon) {
-    if (pokemon.height > 1.0) {
-           document.write(pokemon.name + "." + ` Height: ${pokemon.height}` + " Wow, that’s big!"+ "<br>") 
-    } else {
-        document.write(pokemon.name + "." + ` Height: ${pokemon.height}` +  "<br>")
-    }
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: "Zubat",
+			height: 0.8,
+			types: ["Poison", "Flying"],
+		},
+		{
+			name: "Growlithe",
+			height: 0.7,
+			types: ["Fire"],
+		},
+		{
+			name: "Cubone",
+			height: 0.4,
+			types: ["Ground"],
+		},
+		{
+			name: "Aerodactyl",
+			height: 1.8,
+			types: ["Flying", "Rock"],
+		},
+	];
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+	function getAll() {
+		return pokemonList;
+	}
+	return {
+		add: add,
+		getAll: getAll,
+	};
 })();
+
+pokemonRepository.add({
+	name: "Scyther",
+	height: 1.5,
+	types: ["Bug", "Flying"],
+});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+	if (pokemon.height > 1.0) {
+		document.write(
+			pokemon.name +
+				"." +
+				` Height: ${pokemon.height}` +
+				" Wow, that’s big!" +
+				"<br>"
+		);
+	} else {
+		document.write(pokemon.name + "." + ` Height: ${pokemon.height}` + "<br>");
+	}
+});
